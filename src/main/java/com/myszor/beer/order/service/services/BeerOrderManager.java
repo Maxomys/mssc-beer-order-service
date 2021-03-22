acdad9b79fd6dbfd2e6f87a7db1e07ddf19b99f0
Zaphod <mistrz7losos@live.com>
2021-03-22 03:18:17 +0100

    Added validation result listener.

--- src/main/java/com/myszor/beer/order/service/services/BeerOrderManager.java
+++ src/main/java/com/myszor/beer/order/service/services/BeerOrderManager.java
@@ -2,8 +2,12 @@ package com.myszor.beer.order.service.services;
 
 import com.myszor.beer.order.service.domain.BeerOrder;
 
+import java.util.UUID;
+
 public interface BeerOrderManager {
 
     BeerOrder newBeerOrder(BeerOrder beerOrder);
+
+    void processValidationResult(UUID beerOrderId, boolean isValid);
 
 }
